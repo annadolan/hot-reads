8524c07bfc394da1562b24a18c050ae61bb5ccd2
Anna Dolan <anna.dolan@gmail.com>
2017-02-13 15:43:33 -0700

    Add a bunch of controllers

---
+++ app/controllers/api/v1/links_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::LinksController < ApiController
+  def create
+    @link = Link.find_or_initialize_by(link_params)
+    @link.read_number += 1
+    if @link.save
+      render json: @link, status: 200
+    else
+      render status: 400
+    end
+  end
+
+  private
+    def link_params
+      params.require(:link).permit(:url)
+    end
+end
